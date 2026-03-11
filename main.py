70c3d90fc21a12003afd1d89a590f604cabfd2f4
Sten-22 <85373026+Sten-22@users.noreply.github.com>
2026-03-11 10:21:45 +0100

    config file gemaakt en geimplementeerd in main

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import matplotlib.pyplot as plt
 from serverfunctie import start_server
 from plotgraph import app
+import configparser
+
+config = configparser.ConfigParser()
+config.read("config.ini")
+
+xml_rpc_port = config.getint("network", "port")
 
 def main():
     #start_web_server()
@@ -10,4 +16,4 @@ def main():
 
 if __name__ == "__main__":
     main()
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=xml_rpc_port)
